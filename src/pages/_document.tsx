158a411219a81cd9c12f0bed6fc5dc67c0c54d3a
Wiktoria <wiktoria@Wiktorias-MacBook-Air.local>
2025-05-28 11:52:03 +0200

    fixed analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Analytics } from "@vercel/analytics/next"
 
 export default function Document() {
   return (
@@ -21,7 +20,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Analytics />
       </body>
     </Html>
   );
